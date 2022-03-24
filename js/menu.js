7bb9122e62f8a277f2117e3c08bdbc3a90bd1e19
Raxmice <93811296+Raxmice@users.noreply.github.com>
2022-03-24 16:23:58 +0530

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+function ul1(){
+    var element = document.getElementById("sub-1");
+   element.classList.toggle("tog");
+}
+function ul2(){
+    var element = document.getElementById("sub-2");
+   element.classList.toggle("tog");
+}
+function ul3(){
+    var element = document.getElementById("sub-3");
+   element.classList.toggle("tog");
+}
